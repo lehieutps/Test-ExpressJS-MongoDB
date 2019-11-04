aeb2ee09a22443ae7741fcb50fa37713a5d8b927
hieu.le <hieu.le@tpssoft.com>
2019-11-04 10:08:15 +0700

    add git ignore

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ var router = express.Router();
 
 app.get('/', (req,res) => {res.send('Hello World!')});
 
-app.put('/user', function (req, res) {
-    res.send('Got a PUT request at /user')
-  })
+// app.post('/', function (req, res) {
+//   res.send('Got a POST request')
+// })
+
+// app.put('/user', function (req, res) {
+//   res.send('Got a PUT request at /user')
+// })
+
+// app.delete('/user', function (req, res) {
+//   res.send('Got a DELETE request at /user')
+// })
 
 app.listen(port, () => console.log(`Example app listening on ${port}`));
